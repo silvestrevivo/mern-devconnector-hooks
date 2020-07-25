4b471ee9bdcbc5ee2cf8d38a6ee77940dcd3c657
silvestrevivo <silvestrevivomillan@gmail.com>
2020-07-25 15:45:56 +0200

    get all profiles endpoint

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -107,5 +107,14 @@ router.post('/', [
 // @route   GET api/profile
 // @desc    Get all profiles
 // @access  Public
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+    res.json(profiles);
+  } catch (error) {
+    console.log('error', error.message);
+    res.status(500).send('Server error')
+  }
+})
 
 module.exports = router;
